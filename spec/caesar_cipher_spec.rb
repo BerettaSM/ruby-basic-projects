cd666a6eead261f174082c24731b2cbf105048c7
BerettaSM <ramonsaviato@hotmail.com>
2023-12-21 23:27:46 -0300

    test: add test for correct cipher

--- spec/caesar_cipher_spec.rb
+++ spec/caesar_cipher_spec.rb
@@ -1,5 +1,8 @@
-require '../caesar_cipher.rb'
+require './caesar_cipher.rb'
 
 describe "#caesar_cipher" do
-    
+    it "should correctly cipher" do
+        result = caesar_cipher("What a string!", 5)
+        expect(result).to eql("Bmfy f xywnsl!")
+    end
 end
